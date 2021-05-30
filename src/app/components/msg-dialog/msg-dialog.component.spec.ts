f2afc26dd162d2a479a8f3238530ec4f242e253e
ocknamo <s14pes@gmail.com>
2021-05-30 20:53:40 +0900

    fix(test): test error

--- src/app/components/msg-dialog/msg-dialog.component.spec.ts
+++ src/app/components/msg-dialog/msg-dialog.component.spec.ts
@@ -1,3 +1,4 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
@@ -22,6 +23,7 @@ describe('MsgDialogComponent', () => {
       ],
       declarations: [MsgDialogComponent],
       imports: [MatCheckboxModule],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
   });
 
